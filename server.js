d45053027aeb1d1bef03834ba37275d97cc3e4d8
newal-dev <newalelias71@gmail.com>
2026-04-25 12:31:25 +0300

    Added function to read movies from JSON file

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const fs = require('fs');
 const path=require('path');
 
 const dataFilePath=path.join(__dirname, 'data.json');
+
+function readMoviesFromFile(){
+    try{
+        const data = fs.readFileSync(dataFilePath, 'utf8');
+        const parsed= JSON.parse(data);
+        return parsed.movies || [];
+    }
+    catch(error){
+        console.error('Error reading file:', error);
+        return [];
+    }
+}
