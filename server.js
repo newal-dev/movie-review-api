0b517ee09a9fb497782f307748c7584f02880b86
newal-dev <newalelias71@gmail.com>
2026-04-26 14:07:26 +0300

    Add GET all movies, GET by ID, and POST create movie endpoints. Implement PUT method to update existing movies

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path=require('path');
 
 const dataFilePath=path.join(__dirname, 'data.json');
-//R
+//Read
 function readMoviesFromFile(){
     try{
         const data = fs.readFileSync(dataFilePath, 'utf8');
@@ -16,15 +16,89 @@ function readMoviesFromFile(){
     }
 }
 
-//C
-function writeMoviesformFile(movies){
+//Create
+function writeMoviesToFile(movies){
     try{
-        const data = JSON.stringify({movies: movies}, null 2);
+        const data = JSON.stringify({movies: movies}, null, 2);
         fs.writeFileSync(dataFilePath,data, 'utf8');
         return true;
     }
-    catch{
-        console.error("Error wrting file:", error);
+    catch(error){
+        console.error("Error writing file:", error);
         return false;
     }
 }
+
+//create a server
+const server = http.createServer((req, res) =>{
+    res.setHeader('Content-Type', 'application/json');
+
+    //to get all movies
+    if(req.url === '/movies' && req.method === 'GET'){
+        const movies = readMoviesFromFile();
+        res.end(JSON.stringify(movies));
+    }
+
+    //to get movies by ID
+    else if(req.url.match(/^\/movies\/([0-9]+)$/) && req.method === 'GET'){
+        const id = parseInt(req.url.split('/')[2]);
+        const movies = readMoviesFromFile();
+        const movie = movies.find(b => b.id === id);
+
+        if (movie){
+            res.end(JSON.stringify(movie));
+        }
+        else{
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Movie Not Found');
+        }
+    }
+
+    //to create new movie
+    else if (req.url === '/movies' && req.method === 'POST') {
+        let body = '';
+
+        req.on('data',chunk =>{
+            body+= chunk.toString();
+        });
+
+        req.on('end', () => {
+            const movies = readMoviesFromFile();
+            const newMovie = JSON.parse(body);
+
+            newMovie.id= movies.length > 0 ? movies[movies.length -1].id + 1: 1;
+
+            movies.push(newMovie);
+            writeMoviesToFile(movies);
+
+            res.writeHead(201, {'Content-Type': 'application/json' });
+            res.end(JSON.stringify(newMovie));
+        });
+    }
+
+    //PUT - Update
+    else if (req.url.match(/^\/movies\/([0-9]+)$/) && req.method === 'PUT'){
+        const id = parseInt(req.url.split('/')[2]);
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', () => {
+            const movies = readMoviesFromFile();
+            const updatedData = JSON.parse(body);
+            const index = movies.findIndex(m => m.id === id);
+
+            if(index !==-1){
+                movies[index] = { ...movies[index], ...updatedData };
+                writeMoviesToFile(movies);
+                res.end(JSON.stringify(movies[index]));
+            }
+            else{
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Movie Not Found');
+            }
+        });
+    }
+})
